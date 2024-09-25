f0bbf9d3ef2416dfe5bfcb7246f4f7f988902225
Joo-hyun-Kang <autoba9687@gmail.com>
2024-09-25 21:44:57 +0900

    !HOTFIX: コントローラーにバリデーションの適用のため、main.tsにグローバル設定追加 ＊main.tsにこの設定が省ければ、class-validatorが無視される Issues: #19

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // NestFactoryはアプリケーションインスタンスの作成を可能にするいくつかの静的メソッドを含まれている
   // 多分、モジュールを読んで登録した機能を実行する
   const app = await NestFactory.create(AppModule);
+
+  // ValidationPipeをグローバルに適用
+  app.useGlobalPipes(new ValidationPipe());
 
   // CORSを有効にする
   app.enableCors({
